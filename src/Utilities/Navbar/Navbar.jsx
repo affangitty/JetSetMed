eebf9a5d12c23399a68f89f2ba7bc4bbcffb8509
Affan Ahammed <116718291+affangitty@users.noreply.github.com>
2024-07-30 03:01:53 +0530

    feat: add navbar text

--- src/Utilities/Navbar/Navbar.jsx
+++ src/Utilities/Navbar/Navbar.jsx
@@ -7,7 +7,13 @@ const Navbar = () => {
     <div className="navright">
       <div className="notif">
       </div>
-      <div className="profile"></div>
+      <div className="profile">
+        <div className="pfp"></div>
+        <div className="pinfo">
+          <h3>Gagan N Banganagiri</h3>
+        <div className="profbutton"><p>Premium</p></div>
+        </div>
+      </div>
     </div>
   </div>;
 };
