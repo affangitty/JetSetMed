a973f623f5fd2d40cc08c96c668d9c2455cfec56
Affan Ahammed <116718291+affangitty@users.noreply.github.com>
2024-07-30 02:40:35 +0530

    feat: add navbar

--- src/Utilities/Navbar/Navbar.jsx
+++ src/Utilities/Navbar/Navbar.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className="navbar">
+    <h1>Dashboard</h1>
+    <div className="navright">
+      <div className="notif">
+      </div>
+      <div className="profile"></div>
+    </div>
+  </div>;
 };
 
 export default Navbar;
