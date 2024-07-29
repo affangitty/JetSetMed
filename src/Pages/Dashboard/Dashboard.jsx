1df9fa2d9717ddf25468c906885a68fe1aaed7b1
Siddharth Jain <siddharthj1405@gmail.com>
2024-07-30 01:47:18 +0530

    feat : create website tree and define color root

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Dashboard.css";
+import Navbar from "../../Utilities/Navbar/Navbar";
+import Sidebar from "../../Utilities/Sidebar/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Dashboard;
